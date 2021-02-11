009c90f913905e7999eb224a34c5f4b71d5060a2
wrist <stoicheia1986@gmail.com>
2021-02-12 00:46:42 +0900

    Fix cids.h

--- {{cookiecutter.PLUGIN_NAME_CLI}}/source/{{cookiecutter.PREFIX_FOR_FILENAMES_CLI}}cids.h
+++ {{cookiecutter.PLUGIN_NAME_CLI}}/source/{{cookiecutter.PREFIX_FOR_FILENAMES_CLI}}cids.h
@@ -9,7 +9,7 @@
 
 namespace {{cookiecutter.VENDOR_NAMESPACE_CLI}} {
 //------------------------------------------------------------------------
-static const Steinberg::FUID k{{cookiecutter.PLUGIN_CLASS_NAME_CLI}}ProcessorUID (0x00000000, 0x11111111, 0x22222222, 0x33333333});
+static const Steinberg::FUID k{{cookiecutter.PLUGIN_CLASS_NAME_CLI}}ProcessorUID (0x00000000, 0x11111111, 0x22222222, 0x33333333);
 static const Steinberg::FUID k{{cookiecutter.PLUGIN_CLASS_NAME_CLI}}ControllerUID (0x44444444, 0x55555555, 0x66666666, 0x77777777);
 
 #define {{cookiecutter.PLUGIN_CLASS_NAME_CLI}}VST3Category "{{cookiecutter.PLUGIN_CATEGORY_CLI}}"
