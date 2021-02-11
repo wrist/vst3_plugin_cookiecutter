c37197934f9b3c59f0b24440a051d15fdf8faa4d
wrist <stoicheia1986@gmail.com>
2021-02-12 00:30:03 +0900

    Fix cookiecutter.json. Add post hook to tell uuid generation requirement

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+
+print("")
+print("New project {{ cookiecutter.PLUGIN_NAME_CLI }} is generated!")
+print("Please generate two uuids manually (ex. `uuidgen`) and replace uids in {{ cookiecutter.PLUGIN_NAME_CLI }}/source/{{ cookiecutter.PREFIX_FOR_FILENAMES_CLI }}cids.h with that.")
